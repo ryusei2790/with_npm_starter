15fe3c884e53bb8454f8788931830653d29ca975
ryusei2790 <ryu.chn1978@gmail.com>
2025-07-24 11:50:14 +0900

    Implement country selection feature in HTML and JavaScript. Added a button to trigger a random country selection from a predefined list and display the result in an alert. Updated the main structure of the HTML to accommodate this new functionality while removing unused sections for a cleaner layout.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
+// "use strict";
+document.addEventListener('DOMContentLoaded', function() {
+    let countryItems = ["中華", "イタリアン", "和食", "アメリカ"];
+    const selectCountryButton = document.getElementById("selectCountryButton");
+    let country = document.getElementById("country");
+    let number = 0;
 
+    console.log(countryItems);
+    console.log(selectCountryButton);
+    console.log(country);
+    console.log(number);
+
+    selectCountryButton.addEventListener("click", function(){
+        console.log("クリックされました");
+        let number = Math.floor(Math.random() * countryItems.length);
+        //結果を記録して同じのは出ないようにしたい
+        console.log("結果取得", number);
+        alert(countryItems[number]);
+    });
+});
