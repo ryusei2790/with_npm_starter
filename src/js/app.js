15ad8a69a5de8deb8304c76a30b07a339e9c620d
ryusei2790 <ryu.chn1978@gmail.com>
2025-07-26 16:39:59 +0900

    Remove unused HTML files (howtuuse.html and serachLanch.html) and update package dependencies to include AOS for animations. Adjust scripts in existing HTML files to ensure proper functionality and integration of new features.

--- src/js/app.js
+++ src/js/app.js
@@ -39,3 +39,22 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('結果取得：', result);
     });
 });
+
+
+
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
