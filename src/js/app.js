ad58bc1d6fece8af88cb62bb94503d593795ddcc
ryusei2790 <ryu.chn1978@gmail.com>
2025-07-26 11:45:14 +0900

    Add new HTML pages for how-to-use, search, and registration sections. Update existing pages with improved structure and navigation links. Enhance JavaScript functionality for country selection and update CSS for better layout and responsiveness across the application.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,26 @@
-// "use strict";
+// // "use strict";
+// document.addEventListener('DOMContentLoaded', async function() {
+//     console.log("DOM読み込み完了");
+  
+//     // 3秒待つPromise関数
+//     function delay(ms) {
+//       return new Promise(resolve => setTimeout(resolve, ms));
+//     }
+  
+//     // 非同期関数で待機
+//     await delay(30000);
+  
+//     const overlay = document.getElementById('loadingOverlay');
+//     overlay.style.display = 'none';
+//     console.log("オーバーレイ非表示");
+//   });
+
 document.addEventListener('DOMContentLoaded', function() {
     let countryItems = ["中華", "イタリアン", "和食", "アメリカ"];
     const selectCountryButton = document.getElementById("selectCountryButton");
-    let country = document.getElementById("country");
+    let country = document.getElementById("selectedCountry");
     let number = 0;
+    let selectedIndexes = [];
 
     console.log(countryItems);
     console.log(selectCountryButton);
@@ -15,6 +32,10 @@ document.addEventListener('DOMContentLoaded', function() {
         let number = Math.floor(Math.random() * countryItems.length);
         //結果を記録して同じのは出ないようにしたい
         console.log("結果取得", number);
-        alert(countryItems[number]);
+        // alert(countryItems[number]);
+        selectedIndexes.push(number);//ここの値を出力結果のランダム性に反映させたい
+        const result = countryItems[number];
+        country.textContent = result;
+        console.log('結果取得：', result);
     });
 });
